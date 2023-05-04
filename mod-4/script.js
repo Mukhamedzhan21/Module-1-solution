294ad3a0b439227d2aa15c2fe556edc0ebd479bb
Mukhamedzhan Zhumageldiyev <mukhamedzhanzhu@gmail.com>
2023-05-04 23:44:41 +0600

    aaa.

--- mod-4/script.js
+++ mod-4/script.js
@@ -60,7 +60,7 @@ for (var i = 0; i < names.length; i++) {
   if (firstLetter === 'j') {
     byeSpeaker.speak(names[i]);
   } else {
-    hellospeaker.speak(names[i]);
+    helloSpeaker.speak(names[i]);
   }
 }
 
